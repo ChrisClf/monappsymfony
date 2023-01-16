78f44800b92c3cfad043656715244f5c327249f1
CHRIS <chris@clerfayt.com>
2023-01-16 14:16:08 +0100

    ajout d’un fichier

---
+++ src/Entity/Product.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $titre = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 2, scale: 2)]
+    private ?string $price = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 6, scale: 2)]
+    private ?string $prix = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $quantité = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $articleBlog = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getPrice(): ?string
+    {
+        return $this->price;
+    }
+
+    public function setPrice(string $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getPrix(): ?string
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(string $prix): self
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getQuantité(): ?string
+    {
+        return $this->quantité;
+    }
+
+    public function setQuantité(string $quantité): self
+    {
+        $this->quantité = $quantité;
+
+        return $this;
+    }
+
+    public function getArticleBlog(): ?string
+    {
+        return $this->articleBlog;
+    }
+
+    public function setArticleBlog(string $articleBlog): self
+    {
+        $this->articleBlog = $articleBlog;
+
+        return $this;
+    }
+}
